b74e112e072ca02855df42818b82b9a9ea1e5d38
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-05 12:29:52 -0500

    created a file for general info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Overview from './components/Overview';
 import './App.css';
+import GeneralInfo from './components/GeneralInfo';
 
 function App() {
   return (
     <div className="App">
+      <GeneralInfo />
      <Overview />
     </div>
   );
