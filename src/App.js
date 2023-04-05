62df64078d3f103302ddbc68e943a52ab3b07655
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-05 12:33:07 -0500

    employment history component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Overview from './components/Overview';
 import './App.css';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
+import Employment from './components/Employment';
 
 function App() {
   return (
     <div className="App">
       <GeneralInfo />
       <Education />
+      <Employment />
      <Overview />
     </div>
   );
