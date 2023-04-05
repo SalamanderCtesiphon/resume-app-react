c943ae14f05a662e9ae535b3b5d8ed797ba4f8ea
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-05 12:38:29 -0500

    added a header and a footer component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Employment from './components/Employment';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <GeneralInfo />
       <Education />
       <Employment />
      <Overview />
+     <Footer />
     </div>
   );
 }
