9a95c55b5489817b7e0e88b6561e6487984ef508
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-05 12:31:06 -0500

    education section of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Overview from './components/Overview';
 import './App.css';
 import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
 
 function App() {
   return (
     <div className="App">
       <GeneralInfo />
+      <Education />
      <Overview />
     </div>
   );
